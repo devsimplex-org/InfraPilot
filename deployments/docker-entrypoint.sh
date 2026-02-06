54749e2cdda54c31b12670aac3c0272cbbc69094
tybali <tayyabalihere@gmail.com>
2026-02-06 09:14:25 +0000

    feat: Timescale db apache license operations

--- deployments/docker-entrypoint.sh
+++ deployments/docker-entrypoint.sh
@@ -186,14 +186,14 @@ http {
     default_type application/octet-stream;
 
     # Standard combined log format
-    log_format main '\$remote_addr - \$remote_user [\$time_local] "\$request" '
-                    '\$status \$body_bytes_sent "\$http_referer" '
-                    '"\$http_user_agent" "\$http_x_forwarded_for"';
+    log_format main '$remote_addr - $remote_user [$time_local] "$request" '
+                    '$status $body_bytes_sent "$http_referer" '
+                    '"$http_user_agent" "$http_x_forwarded_for"';
 
     # Extended log format for InfraPilot analytics (includes request_time and host)
-    log_format infrapilot_analytics '\$remote_addr - \$remote_user [\$time_local] "\$request" '
-                                    '\$status \$body_bytes_sent "\$http_referer" '
-                                    '"\$http_user_agent" \$request_time "\$host" "\$upstream_addr"';
+    log_format infrapilot_analytics '$remote_addr - $remote_user [$time_local] "$request" '
+                                    '$status $body_bytes_sent "$http_referer" '
+                                    '"$http_user_agent" $request_time "$host" "$upstream_addr"';
 
     access_log /var/log/nginx/access.log infrapilot_analytics;
 
